bbeedb86c2331131bc42aa81db858b2d87f1d360
JbRezidu <duriez.jeanbaptiste@gmail.com>
2016-04-26 12:00:08 +0200

    Couleur des murs

--- tpastar2016-2/tpastar2016/src/algo/actions/Neighborhoud8Dir.java
+++ tpastar2016-2/tpastar2016/src/algo/actions/Neighborhoud8Dir.java
@@ -22,9 +22,12 @@ public class Neighborhoud8Dir implements ComputeNeighborhood {
 				int yj = y + j;
 				if (xi >= 0 && xi < ent.getWidth() && yj >= 0 && yj < ent.getHeight()) {
 					Cell v = ent.getCell(xi, yj);
-					if (!v.isContainer()) {
-						r.add(v);
-					}
+					//Dans les exercices avancés, on peut passer à travers les murs
+					//On ne check donc plus si la cellule est un mur ou non
+//					if (!v.isContainer()) {
+//						r.add(v);
+//					}
+					r.add(v);
 				}
 			}
 		}
